74a0854c75b8aa34feea5f49756f9530b657544d
Ayatullah Ma'arif <aya.mf12@gmail.com>
2024-08-16 15:08:17 +0700

    chore: init xcode project

---
+++ FlippyMath/FlippyMathApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlippyMathApp.swift
+//  FlippyMath
+//
+//  Created by Ayatullah Ma'arif on 16/08/24.
+//
+
+import SwiftUI
+
+@main
+struct FlippyMathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
